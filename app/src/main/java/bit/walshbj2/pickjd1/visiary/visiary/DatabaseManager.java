a2362a8c5f3afc8999ebb23dbab2e9f0ac1718f5
gb3itdevs <gb3itdevs@gmail.com>
2015-05-23 18:54:55 +1200

    Fixed minor spelling errors in database manager

--- app/src/main/java/bit/walshbj2/pickjd1/visiary/visiary/DatabaseManager.java
+++ app/src/main/java/bit/walshbj2/pickjd1/visiary/visiary/DatabaseManager.java
@@ -12,19 +12,19 @@ public class DatabaseManager extends SQLiteOpenHelper{
     public static final String TABLE_NAME = "tblJournalEntries";
     public static final String COLUMN_ID = "journalID";
     public static final String COLUMN_DATE = "date";
-    public static final String COLUMN_LOCATION = "loaction";
+    public static final String COLUMN_LOCATION = "location";
     public static final String COLUMN_PIC_FILEPATH = "picFilePath";
     public static final String COLUMN_BLURB = "blurb";
 
-    private static final String DATABASE_NAME = "journalEntree.db";
+    private static final String DATABASE_NAME = "journalEntry.db";
     private static final int DATABASE_VERSION = 1;
 
     //Database creation sql statement
     private static final String createQuery = "CREATE TABLE "
-            + TABLE_NAME + "("+COLUMN_ID + "INEGER PRIMARY KEY AUTOINCREMENT, "
+            + TABLE_NAME + "("+COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + COLUMN_DATE + " DATE NOT NULL, "
             + COLUMN_LOCATION + " TEXT NOT NULL, "
-            + COLUMN_PIC_FILEPATH + " BLOB NOT NULL, "
+            + COLUMN_PIC_FILEPATH + " TEXT NOT NULL, "
             + COLUMN_BLURB + " TEXT NOT NULL);" ;
 
     public DatabaseManager(Context context) {
