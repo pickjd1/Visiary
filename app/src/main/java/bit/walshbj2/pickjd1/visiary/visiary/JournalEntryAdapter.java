e05b85ade697f7a19bb9164ed6025a5dad074c95
gb3itdevs <gb3itdevs@gmail.com>
2015-05-24 12:53:01 +1200

    Date Time formatted to make readable on Home page, colour added to date and location.

--- app/src/main/java/bit/walshbj2/pickjd1/visiary/visiary/JournalEntryAdapter.java
+++ app/src/main/java/bit/walshbj2/pickjd1/visiary/visiary/JournalEntryAdapter.java
@@ -11,6 +11,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class JournalEntryAdapter extends ArrayAdapter<JournalEntry>{
@@ -43,11 +46,33 @@ public class JournalEntryAdapter extends ArrayAdapter<JournalEntry>{
         //Get the current inputItem
         JournalEntry currentEntry = getItem(position);
 
+        //Format Date
+        //Create a new formatted timestamp showing todays date and time
+
+        String date = currentEntry.getDate();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd_HHmmss");
+
+        Date formatInDateFrom = null;
+        // Parse String date to Date
+        try {
+            formatInDateFrom = (Date) formatter.parse(date);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        // Set up new format ("WeekDay, Day, Month ' Year Hour:Mintues M")
+        SimpleDateFormat newFormat = new SimpleDateFormat(
+                "EEE, d MMM, ''yy h:mm aaa");
+        // reformat date and put back into a string
+        String formattedDate = newFormat.format(formatInDateFrom);
+
+        //Get the filePath of the image, make it into a bitmap.
         String filePath = currentEntry.getPicFilePath();
         Bitmap userPhoto = BitmapFactory.decodeFile(filePath);
 
         //Use the instance data to initialise the view controls
-        dateTV.setText(currentEntry.getDate());
+        dateTV.setText(formattedDate);
         locationTV.setText(currentEntry.getLocation());
         picIV.setImageBitmap(userPhoto);
         blurbTV.setText(currentEntry.getBlurb());
