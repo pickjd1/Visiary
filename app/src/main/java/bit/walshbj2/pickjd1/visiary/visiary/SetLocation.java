27abeecca65978bab492a0d344fd409dee8a4963
gb3itdevs <gb3itdevs@gmail.com>
2015-06-12 16:42:27 +1200

    Final Final Version

--- app/src/main/java/bit/walshbj2/pickjd1/visiary/visiary/SetLocation.java
+++ app/src/main/java/bit/walshbj2/pickjd1/visiary/visiary/SetLocation.java
@@ -101,11 +101,6 @@ public class SetLocation extends FragmentActivity  implements
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
